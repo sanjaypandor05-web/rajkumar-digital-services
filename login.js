6d6fd6c8bffa8f888ba51eed28f4e72f05f3841e
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-14 18:34:41 +0530

    Update login.js

--- login.js
+++ login.js
@@ -8,9 +8,8 @@
    GOOGLE APPS SCRIPT URL
    ADMIN LOGIN ALREADY WORKING - DO NOT CHANGE
 ===================================================== */
-
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbxzur__8Lzuis0fQxWtyhjW6XiB6XNC72yxuBSyuv0tOO2PSZQeS45Ssup5F-AlAGGhGQ/exec";
+"https://script.google.com/macros/s/AKfycbw1mKC92_EjWJS_x2o8LMqiL9sssMbFh089IhMujZLd6_9VuujoVckjoMS8fbajVn-uQQ/exec";
 /* =====================================================
    ELEMENTS
 ===================================================== */
