2618ddcf1ce1659966da8d839e8f80f250c5508b
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-18 08:23:51 +0530

    Update apply.js

--- apply.js
+++ apply.js
@@ -12,7 +12,7 @@
  * const GOOGLE_SCRIPT_URL = "https://script.google.com/...";
  */
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwN-xev_9yNqkrce0rnZ-ePCwQ3wkqvQC1brT0CncJv4ce8yv7LuaKcuLojQKfMuaHS/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx9BpKuN0xukn1kcqPIndRkYDOxaBAmgsI_4laS2cPdPhUxGc8P2Fz_R69-uwcTSHNm0A/exec";
 
 
 /* UPI */
