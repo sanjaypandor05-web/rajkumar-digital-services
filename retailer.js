b5035db11f6d15f49e574198542076764e273eec
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-18 21:42:56 +0530

    Update retailer.js

--- retailer.js
+++ retailer.js
@@ -5,7 +5,7 @@
 ===================================================== */
 
 const SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbzVyarGuWcFfauDBpPmD4d6xUak9MmINfcUGAbz1JrxA6s-n3nQOBUQszQxFQKsz25Iow/exec";
+"https://script.google.com/macros/s/AKfycbx_Jlr04g2fJl76vXnuq2-jS4P3PPrb-p3RkrE-YZ4MMeHgygQQSutjR05xvKTC9yhu/exec";
 
 
 /* =====================================================
