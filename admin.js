9fa1d08ce44f8f83626a7a592588593696283b1d
sanjaypandor05-web <sanjaypandor05@gmail.com>
2026-08-19 23:10:05 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -25,7 +25,7 @@
    ============================================================ */
 
 const API_URL =
-  "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+  "https://script.google.com/macros/s/AKfycbzleZG-w2WQ6DClkEcqpRcn6Pv8gil3ym-aP4_9ctLUlzeiHG34MyDQgdV6JMK1r4zLnA/exec";
 
 
 /* ============================================================
